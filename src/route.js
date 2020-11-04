c356e64d6ae756f04306d3affd2198cb21683add
yasinsulak61 <2014010205082@ogrenci.karabuk.edu.tr>
2020-11-04 09:23:03 +0300

    First Commit

--- src/route.js
+++ src/route.js
@@ -4,6 +4,7 @@ const SignUp = React.lazy(() => import('./Pages/Authentication/SignUp/SignUp'));
 const Signin = React.lazy(() => import('./Pages/Authentication/SignIn/SignIn'));
 
 const route = [
+    
     { path: '/auth/signup', exact: true, name: 'Signup', component: SignUp },
     { path: '/auth/signin', exact: true, name: 'Signin', component: Signin }
 ];
